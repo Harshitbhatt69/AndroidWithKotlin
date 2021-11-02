cbf485ad92ba55ad9701cfa0db4db64d9187b898
Harshit <harshitbhatt972@gmail.com>
2021-11-03 02:01:13 +0530

    debugging successful

--- pract/app/src/main/java/com/example/pract/MainActivity.kt
+++ pract/app/src/main/java/com/example/pract/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.memeapk
+package com.example.pract
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,14 +7,13 @@ import android.view.View
 import android.widget.ImageView
 import android.widget.Toast
 import com.android.volley.Request
+import com.android.volley.RequestQueue
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
-import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.bumptech.glide.Glide
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -27,9 +26,10 @@ class MainActivity : AppCompatActivity() {
         val queue = Volley.newRequestQueue(this)
         val url = "https://meme-api.herokuapp.com/gimme"
 
-        val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, url, null, Response.Listener { response ->
-//            Log.i("error", response.toString())
+        val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, url, null,
+        Response.Listener { response ->
             val imgUrl = response.getString("url")
+//            Log.i("url", imgUrl)
             val ivMeme = findViewById<ImageView>(R.id.ivMeme)
             Glide.with(this).load(imgUrl).into(ivMeme)
         },
@@ -40,26 +40,6 @@ class MainActivity : AppCompatActivity() {
         queue.add(jsonObjectRequest)
     }
 
-    fun onClickNext(view: View) {
-        loadMore()
-    }
     fun onClickShare(view: View) {}
-    fun onClickFollow(view: View) {}
+    fun onClickNext(view: View) {}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
